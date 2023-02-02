d4dab151c793e094ad3f0a6e8e0d2238759caa05
mattglbrt <matt@everyway.io>
2023-02-02 14:43:05 -0500

    Social Media Links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,15 @@ import mdx from '@astrojs/mdx';
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({
 
+// https://astro.build/config
+export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
     extendDefaultPlugins: true,
-    drafts: true,
+    drafts: true
   },
-
   integrations: [mdx({
-    drafts: true,
-  }),
-
-    [preact()]
-  ]
+    drafts: true
+  }), [preact()]]
 });
